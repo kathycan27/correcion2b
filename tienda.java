96a13aba94d121001652ec25a2485e5f88dcfb0e
kathycan27 <katherine.cangahuamin@epn.edu.ec>
2023-02-24 16:45:44 -0500

    corre

--- tienda.java
+++ tienda.java
@@ -1,22 +1,97 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
 
 public class tienda {
     private JPanel panel1;
-    private JTextField txtfruta;
-    private JTextField textField2;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField txtcodigo;
+    private JTextField txtprecio;
+    private JComboBox cbtipo;
+    private JComboBox cbdetails;
+    private JTextField txtunidades;
     private JButton crearButton;
     private JButton buscarButton;
     private JButton btnactualizar;
     private JButton borrarButton;
     private JLabel codigo;
-    private JLabel txtnombre;
+    private JLabel Tipo;
+    private JTextField txtnombre;
+    PreparedStatement st;
+    String tabla, valor;
+    JComboBox combo;
+    Connection con;
+    Statement statement;
+    ResultSet rs;
+    public void rellenarTipos(String tabla, String valor,JComboBox combo)
+    {
+        String sql="select * from "+tabla;
+        try {
+            con=getConection();
+            statement=con.createStatement();
+            while (rs.next())
+            {
+                combo.addItem(rs.getString(valor));
+            }
+        }catch (SQLException g)
+        {
+            JOptionPane.showMessageDialog(null, "Error"+ g.toString());
+        }
+    }
+
+    public tienda() {
+
+
+
+    crearButton.addActionListener(new ActionListener() {
+           Connection connection;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                connection = getConection();
+                String tipop = cbtipo.getSelectedItem().toString();
+                String detailsp = cbdetails.getSelectedItem().toString();
+                try{
+                    st = connection.prepareStatement("INSERT INTO product(codigo,nombre,tipo,precio,unidad,details) VALUES (?,?,?,?,?,?)");
+
+                    st.setString(1,txtcodigo.getText());
+                    st.setString(2,txtnombre.getText());
+                    st.setString(4,txtprecio.getText());
+                    st.setString(5,txtunidades.getText());
+                    st.setString(3, tipop);
+                    st.setString(6, detailsp);
+
+                    int res = st.executeUpdate();
+
+                    if(res > 0){
+                        JOptionPane.showMessageDialog(null, "Se creo de manera correta");
+                        txtcodigo.setText("");
+                        txtnombre.setText("");
+                        txtprecio.setText("");
+                        txtunidades.setText("");
+                    }else {
+                        JOptionPane.showMessageDialog(null, "No se pudo crear");
+                    }
+
+                }catch (HeadlessException | SQLException f){
+                    System.out.println(f);
+                }
+
+
+            }
+        });
+        cbtipo.addActionListener(new ActionListener() {
+            Connection con;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                rellenarTipos("tipos",valor,cbtipo);
+
+            }
+        });
+    }
 
     public static Connection getConection() {
         Connection con = null;
